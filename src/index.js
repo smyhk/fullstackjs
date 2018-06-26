ef1773f404c08f480f8cc6d6c113c786bdba984f
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-06-25 17:58:49 -0700

    Define simple React components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const Header = ({ message }) => {
+  return (
+    <h2 className=" Header text-center">
+      {message}
+    </h2>
+  );
+};
+
+// prop validation
+Header.propTypes = {
+  message: PropTypes.string.isRequired  // only accepts a string and is mandatory
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header message="Naming Contests" />
+      <div>
+        ...
+      </div>
+    </div>
+  );
+};
 
 ReactDOM.render(
-  <h2 className="text-center">
-    Hello React with JSX!!
-  </h2>,
+  <App />,
   document.getElementById('root')
 );
