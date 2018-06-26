f10cbe2e46c7bf7b3389bc4cd81f52679e8ffa22
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-06-25 18:13:39 -0700

    Modularize components

--- src/index.js
+++ src/index.js
@@ -1,30 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-
-const Header = ({ message }) => {
-  return (
-    <h2 className=" Header text-center">
-      {message}
-    </h2>
-  );
-};
-
-// prop validation
-Header.propTypes = {
-  message: PropTypes.string.isRequired  // only accepts a string and is mandatory
-};
-
-const App = () => {
-  return (
-    <div className="App">
-      <Header message="Naming Contests" />
-      <div>
-        ...
-      </div>
-    </div>
-  );
-};
+import App from './components/App';
 
 ReactDOM.render(
   <App />,
