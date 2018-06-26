338665b1d913caaa50311c897f2ed86a25e3e7e3
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-06-26 14:10:08 -0700

    Configure Sass preprocessor

--- src/components/ContestPreview.js
+++ src/components/ContestPreview.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import '../ContestPreview.css';
+
 const ContestPreview = (contest) => (
   <div className="ContestPreview">
-    <div>
+    <div className="category-name">
       {contest.categoryName}
     </div>
-    <div>
+    <div className="contest-name">
       {contest.contestName}
     </div>
   </div>
