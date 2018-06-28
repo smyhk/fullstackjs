d0ded84044b79195cea53bb4fdbdb163dbca14de
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-06-27 17:22:33 -0700

    Rename and add start script

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 
 const server = express();
+
+// graphql middleware
+server.use('/graphql', graphqlHTTP({
+  
+}));
 
 server.listen(4000, () => {
   console.info('Express server listening on port 4000');
