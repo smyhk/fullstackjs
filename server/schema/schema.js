4adb748d2882836eb1c709ff3a8587aef0d9ee5b
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-06-27 22:41:33 -0700

    Add basic mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -79,6 +79,43 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+ const Mutation = new GraphQLObjectType({
+   name: 'Mutation',
+   fields: {
+     addAuthor: {
+       type: AuthorType,
+       args: {
+         name: { type: GraphQLString },
+         age: { type: GraphQLInt }
+       },
+       resolve(parent, args) {
+         let author = new Author({
+           name: args.name,
+           age: args.age
+         });
+         return author.save();
+       }
+     },
+     addBook: {
+       type: BookType,
+       args: {
+         title: { type: GraphQLString },
+         genre: { type: GraphQLString },
+         authorId: { type: GraphQLID }
+       },
+       resolve(parent, args) {
+         let book = new Book({
+           title: args.title,
+           genre: args.genre,
+           authorId: args.authorId
+         });
+         return book.save();
+       }
+     }
+   }
+ })
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 });
